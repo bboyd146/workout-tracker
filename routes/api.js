14b9a4cba2a56c08f0e485e9d4f43537133fcdc4
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-09-25 14:43:43 -0500

    added get route for workouts

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,16 @@
 const router = require("express").Router();
 const Exercise = require("../models/Exercise");
 
+router.get("/api/workouts", (req, res)=> {
+    Workout.aggregate([
+        { $addFields: {totalDuration: {$sum: "$exercises.duration"}}}
+    ])
+        .then (workouts => {
+            res.json(workouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
 module.exports = router;
