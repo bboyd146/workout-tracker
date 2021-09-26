86e007299cdd86c13565eaa8ac0847f8b1e2e21b
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-09-25 20:10:08 -0500

    added paths to exercise and stats routes

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Exercise = require("../models/Exercise");
+const path = require('path');
 
 router.get("/api/workouts", (req, res)=> {
     Exercise.aggregate([
@@ -11,6 +12,14 @@ router.get("/api/workouts", (req, res)=> {
         .catch(err => {
             res.status(400).json(err);
         });
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
